7fe4e2c69e91142d0a191a723d61515924a75bfa
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2022-03-27 06:39:54 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,26 @@
+def call(reponame)
+{
+  pipeline{
+    tools{
+        jdk 'myjava'
+        maven 'mymaven'
+    }
+	agent any
+      stages{
+           stage('Checkout'){
+	    
+               steps{
+		 echo 'cloning..'
+                 git ${reponame}
+              }
+          }
+          stage('Compile'){
+             
+              steps{
+                  echo 'compiling..'
+                  sh 'mvn compile'
+	      }
+          }
+      }
+}
+  }
